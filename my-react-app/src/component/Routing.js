7a9b2fa98cce32355a445d24bc9d10c4e2ed26d9
Abhishek1380 <undeabhishek707@gmail.com>
2023-07-14 00:50:20 +0530

    Major Changes done

---
+++ my-react-app/src/component/Routing.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
+
+const Routing = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Main />}>
+                        {/* <Route index element={<Home />} />
+                        <Route path="Home" element={<Home />} /> */}
+                    </Route>
+                </Routes>
+
+                <Footer />
+            </BrowserRouter>
+
+        </>
+    )
+}
+export default Routing;
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => {
-    return (
-        <h1>Hello,I'm react</h1>
-    )
-}
+import Routing from './component/Routing';
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(<Routing />);
