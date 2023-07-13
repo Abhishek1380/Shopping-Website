2fb456c083bb248427c7193cc31534c5a758c46f
Abhishek1380 <undeabhishek707@gmail.com>
2023-07-14 01:52:09 +0530

    Home Section  for routing is done

--- my-react-app/src/component/Header.js
+++ my-react-app/src/component/Header.js
@@ -3,7 +3,51 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <h1>This is Header</h1>
+        <>
+            <header className="Navbar">
+                <div className="Nav">
+                    <div className="Nav-1">
+                        <div className="Nav-1-1">
+                            <a href="#"><img src="https://i.ibb.co/G371csj/SubFile.png" alt="SubFile" border="0" /></a>
+                            <h6>Explore<span>Plus+</span></h6>
+                        </div>
+                    </div>
+                    <div className="Nav-2">
+                        <div className="search-input">
+                            <button className="	fa fa-search"></button>
+                            <input type="text " placeholder="Search for products,brands and more" />
+                        </div>
+                    </div>
+                    <div className="Nav-3">
+                        <div>
+                            <div className="links">
+                                <a href="#" class="fa fa-university icon-margin visible"></a>
+                                <a href="#"> Seller</a>
+                            </div>
+
+                        </div>
+                        <div>
+                            <div className="links">
+                                <a href="#" className="fa fa-user-o icon-margin visible"></a>
+                                <a href="#">Seller</a>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="links">
+                                <a href="#" class="fa fa-shopping-cart icon-margin visible"></a>
+                                <a href="#">Cart</a>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="links">
+                                <a href="#" className="fa fa-ellipsis-v"></a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
     )
 }
 
--- my-react-app/src/component/Routing.js
+++ my-react-app/src/component/Routing.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import Home from './Home/Home';
 import Main from './Main';
 
 const Routing = () => {
@@ -11,8 +12,8 @@ const Routing = () => {
                 <Header />
                 <Routes>
                     <Route path="/" element={<Main />}>
-                        {/* <Route index element={<Home />} />
-                        <Route path="Home" element={<Home />} /> */}
+                        <Route index element={<Home />} />
+                        <Route path="Home" element={<Home />} />
                     </Route>
                 </Routes>
 
